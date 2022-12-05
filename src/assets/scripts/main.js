388ca36c0b0b1db3c5bb6490bf9a6d23eb2685fd
Sara Mansori <sara.mansori@cabify.com>
2022-12-05 22:23:47 +0100

    add: setup

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,18 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any other JavaScript below
+ */
+
++(function () {
+  const university = "UOC";
+  console.log(`Hello, ${university}!`);
+})();
+
+import * as bootstrap from "bootstrap";
